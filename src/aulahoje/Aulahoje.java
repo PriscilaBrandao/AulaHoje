353eb237a149005add8bd21f5cc7ef406e7266ca
PriscilaBrandao <priscilabrandao1987@gmail.com>
2018-11-23 22:18:34 -0200

    commit dois

--- src/aulahoje/Aulahoje.java
+++ src/aulahoje/Aulahoje.java
@@ -16,7 +16,7 @@ public class Aulahoje {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Aula hoje");
+        System.out.println("Complicando");
     }
     
 }
